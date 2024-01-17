fe0e903432a815faf38d0b7e38825aec02ef7d05
Your Name <iammthombeni@gmail.com>
2024-01-17 21:53:04 +0200

    'latest'

--- src/components/home.js
+++ src/components/home.js
@@ -1,4 +1,7 @@
+// Home.js
 import React from 'react';
+
+
 import hotel1 from '../images/hotel1.jpg';
 import gym1 from '../images/gym1.jpg';
 import poolbar from '../images/poolbar.jpg';
@@ -7,83 +10,78 @@ import swimmingpool from '../images/swimmingpool.jpg';
 import restuarent from '../images/restuarent.jpg';
 import laundry from '../images/laundry.jpg';
 
-function Home (){
-    return ( 
-        
-            <div>
-                <table>
-                    <tr>
-                        <td>
-                            <div className = 'img-con'>
-                                <img src={hotel1} className='hotel1' alt='img'/>
-                                <div className = 'nav-container'>
-                                    <nav>
-                                        <div className='navbar-links-container'>
-                                            <a href='home' className='nav-links' style={{fontSize:"25px"}}>Facilities</a>
-                                            <a href='rooms' className='nav-links' style={{fontSize:"25px"}}>Rooms</a>
-                                        </div>
-                                    </nav>
-
-                                </div>
-                                <div className='text-container'>
-                                    <p style={{fontSize:'70px'}}>WELCOME TO</p>
-                                    <h1 style={{fontSize:'120px',fontFamily:'serif'}}>LUXURY</h1>
-                                    <h4 style={{fontSize:'50px',fontFamily:'serif',wordSpacing:'40px'}}>H O T E L S</h4>
-                                    <b style={{fontSize:'18px'}}><p>Book your stay and enjoy Luxury</p>
-                                    <p> redefined at the most affordable rates</p></b>
-                                    
-                                </div>
-                            </div>
-                        </td>
-                    </tr>
-                    <tr classname='App'>
-                        <td>
-                            <div className='facilities'>
-                                <div className='text'>
-                                <h1>FACILITIES</h1>
-                                </div>
-                                <p>We want your stay at out lush hotel to be truly unforgotable. This is why we give special attention to all of your needs so<br/>
-                                &nbsp; that we can ensure an experience quite unique. Luxury hotels offer the the perfect setting with stunning views for leisure<br/>
-                                    &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; and our modern luxury resort facilities will help you enjoy the best of all</p>
-                            <div className='gym1'>
-                            <img src={gym1} className='the gym' alt='img'/>
-                                <h1 className='gym-text'>THE GYM</h1>
-                            </div>
-                        
-                            <div className='poolbar'>
-                            <img src={poolbar} className='poolbar' alt='img' style={{width: '1200px'}}/>
-                                <h1 className='poolside-text'>POOLSIDE BAR</h1>
-                            
-                        
-                            </div><br></br><br></br><br></br>
-
-                            <div className='spa1'>
-                            <img src={spa1} className='spa1' alt='img' style={{width: '1200px'}}/>
-                                <h1 className='spa-text'>THE SPA</h1>
-                            
-                        
-                            </div><br></br><br></br><br></br>
-                            <div className='swimmingpool'>
-                            <img src={swimmingpool} className='swimmingpool' alt='img' style={{width: '1200px'}}/>
-                                <h1 className='pool-text'>SWIMMING POOL</h1>
-                            
-                            </div><br></br><br></br><br></br>
-                            <div className='restuarent'>
-                            <img src={restuarent} className='restuarent' alt='img' style={{width: '1200px'}}/>
-                                <h1 className='restuarent-text'>RESTUARENT</h1>
-                            
-                            </div><br></br><br></br><br></br>
-                            <div className='laundry'>
-                            <img src={laundry} className='laundry' alt='img' style={{width: '1200px'}}/>
-                             <h1 className='laundry-text'>LAUNDRY</h1>
-                            
-                            </div>
-                            </div>
-                        </td>
-
-                    </tr>
-                </table>
+function Home() {
+  return (
+    <div>
+      <div className="img-container">
+        <img src={hotel1} className="hotel1" alt="img" />
+        <div className="nav-container">
+          <nav>
+            <div className="navbar-links-container">
+              <a href="home" className="nav-links" style={{ fontSize: '25px' }}>
+                Facilities
+              </a>
+              <a href="rooms" className="nav-links" style={{ fontSize: '25px' }}>
+                Rooms
+              </a>
             </div>
-        );
-        }
-             export default Home; 
+          </nav>
+        </div>
+        <div className="text-container">
+          <p style={{ fontSize: '70px' }}>WELCOME TO</p>
+          <h1 style={{ fontSize: '120px', fontFamily: 'serif' }}>LUXURY</h1>
+          <h4 style={{ fontSize: '50px', fontFamily: 'serif', wordSpacing: '40px' }}>
+            H O T E L S
+          </h4>
+          <b style={{ fontSize: '18px' }}>
+            <p>Book your stay and enjoy Luxury</p>
+            <p> redefined at the most affordable rates</p>
+          </b>
+        </div>
+      </div>
+
+      <div className="facilities">
+        <div className="text">
+          <h1>FACILITIES</h1>
+        </div>
+        <p>
+            We want your stay at our lush hotel to be truly unforgettable. This is why we givespecial attention to all of your needs so <br/>
+            &nbsp;that we can ensure an experience quite unique. Luxury hotels offer the perfect setting with stunning views for leisure,<br/>
+            &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;and our modern luxury resort facilities will help you enjoy the best of all.
+        </p>
+
+        <div className="gym1">
+          <img src={gym1} className="gym1" alt="img" />
+          <h1 className="gym-text">THE GYM</h1>
+        </div>
+
+        <div className="poolbar">
+          <img src={poolbar} className="poolbar" alt="img" />
+          <h1 className="poolside-text">POOLSIDE BAR</h1>
+        </div>
+
+        <div className="spa1">
+          <img src={spa1} className="spa1" alt="img" />
+          <h1 className="spa-text">THE SPA</h1>
+        </div>
+
+        <div className="swimmingpool">
+          <img src={swimmingpool} className="swimmingpool" alt="img" />
+          <h1 className="pool-text">SWIMMING POOL</h1>
+        </div>
+
+        <div className="restuarent">
+          <img src={restuarent} className="restuarent" alt="img" />
+          <h1 className="restuarent-text">RESTAURANT</h1>
+        </div>
+
+        <div className="laundry">
+          <img src={laundry} className="laundry" alt="img" />
+          <h1 className="laundry-text">LAUNDRY</h1>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
--- src/components/login.js
+++ src/components/login.js
@@ -1,33 +1,43 @@
-import React from 'react';
-import { getAuth, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { getAuth, signInWithEmailAndPassword, sendPasswordResetEmail } from 'firebase/auth';
 import { auth } from '../config/firebase';
-import { useState } from 'react';
-import { useNavigate, useEffect, Link } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
+import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBInput } from 'mdb-react-ui-kit';
 
 import hotel1 from '../images/hotel1.jpg';
-import { MDBBtn, MDBContainer, MDBRow, MDBCol, MDBInput } from 'mdb-react-ui-kit';
 
 function Login() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
-  const navigate = useNavigate(); // Use this to navigate after successful login
+  const navigate = useNavigate();
 
   const handleLogin = () => {
-    const auth = getAuth();
-    signInWithEmailAndPassword(auth, email, password)
+    const authInstance = getAuth();
+    signInWithEmailAndPassword(authInstance, email, password)
       .then(() => {
         alert('You have logged in successfully');
-        navigate('/home'); // Navigate to the desired page after login
+        navigate('/home');
       })
       .catch((error) => {
         alert('Login failed. Check your credentials.');
       });
   };
 
+  const handleForgotPassword = () => {
+    const authInstance = getAuth();
+    sendPasswordResetEmail(authInstance, email)
+      .then(() => {
+        alert('Password reset email sent. Check your inbox.');
+      })
+      .catch((error) => {
+        alert('Error sending password reset email. Check your email address.');
+      });
+  };
+
   return (
     <div className='login-container'>
-      <MDBContainer className='my-5gradient-form'>
+      <MDBContainer className='my-5 gradient-form'>
         <MDBRow>
           <MDBCol col='6' className='mb-5'>
             <div className='d-flex flex-column ms-5'>
@@ -52,7 +62,7 @@ function Login() {
                 <MDBBtn className='mb-4 w-100 gradient-custom-2' onClick={handleLogin}>
                   Login
                 </MDBBtn>
-                <a className='text-muted' href='#!'>
+                <a className='text-muted-link' href='#!' onClick={handleForgotPassword}>
                   Forgot password?
                 </a>
               </div>
